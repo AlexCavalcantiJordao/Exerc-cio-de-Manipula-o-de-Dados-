74e76fa333136aa2aeb9164690af8bdc6e60bb60
Mezengas <alexfonsecaramos@hotmail.com>
2022-12-21 10:44:57 -0300

    nbproject src test build manifest.mf

---
+++ src/exercicio_manipulacao/de/dados/Exercicio_ManipulacaoDeDados.java
@@ -0,0 +1,21 @@
+package exercicio_manipulacao.de.dados;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio_ManipulacaoDeDados {
+
+    /* 1) Fazer um programa para ler o caminho de um arquivo .csv contendo os dados itens vendidos. Cada item possuei
+          um nome, preço unitário e quantidade, separados por vírgula. Você deve gerar um novo arquivo chamado "summary.csv",
+          localizado em uma subpasta chamada "out" a partir da pasta origem, contendo apenas o nome e o valor total 
+           para aquele item (preço unitário multiplicado pela quantidades), conforme exemplo. 
+     */
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
